3257c7a8811c4032a58ae0231e0fb6c1b79e17bc
MichaelBosciglio <55165965+MichaelBosciglio@users.noreply.github.com>
2022-11-19 14:12:00 -0500

    ostream

--- Relation.cpp
+++ Relation.cpp
@@ -8,15 +8,21 @@ using namespace std;
 
 template <typename typex>
 ostream& operator << (ostream& out, const Relation<typex>& s) {
-    typename set <typex>::iterator ssi;
-    out << "{";
-    for (ssi = s.root.begin(); ssi != s.root.end(); ++ssi)
-    {
-        if (ssi == prev(s.root.end())) out << *ssi;
-        else out << *ssi << ", ";
+    // typename set <typex>::iterator si;
+    typename set<pair<typex,typex>>::iterator it;
 
-    }
+    out << "{";
+    for (it = s.relations.begin(); it != s.relations.end(); it++){
+        cout << "(";
+        cout << it->first << "," << it->second;
+        cout << ")";
+        if (s.relations.size() > 1){
+            cout << " ";
+        }
+    }   
     out << "}";
+    
+    
     return out;
 }
 
@@ -54,6 +60,8 @@ bool Relation<type>::add_element(pair<type, type> p) {
 
     relations.insert(p);
     size += 1;
+    return true;
+
 }
 
 template <typename type>
@@ -63,81 +71,66 @@ bool Relation<type>::add_to_set(type i) {
     return true;
 }
 
-template <typename type>
-void Relation<type>::remove_element(pair<type, type> p) {
-    /*relations.erase(p);
-    typename set <pair<type, type>>::iterator pi;
-    for (pi = relations.begin(); pi != relations.end(); ++pi)
-    {
-        if (pi.count(pi.first) < 2 ) root.erase(pi.first);
-        if (pi.count(pi.second) < 2) root.erase(pi.second);
+// template <typename type>
+// void Relation<type>::remove_element(type, type) {
 
-    }
-    root.erase(p);
-    --size;*/
-}
+// }
 
-template <typename type>
-bool Relation<type>::is_member(pair <type, type> p) {
-    if (relations.count(p) > 0) return true;
-    return false;
-}
+// template <typename type>
+// bool Relation<type>::is_member(type, type) {
 
-template <typename type>
-bool Relation<type>::equal(Relation r) {
+// }
 
-}
-template <typename type>
-bool Relation<type>::symmetric() {
+// template <typename type>
+// bool Relation<type>::equal(Relation r) {
 
-}
-template <typename type>
-bool Relation<type>::asymmetric() {
+// }
+// template <typename type>
+// bool Relation<type>::symmetric() {
 
-}
-template <typename type>
-bool Relation<type>::reflexive() {
+// }
+// template <typename type>
+// bool Relation<type>::asymmetric() {
 
-}
-template <typename type>
-bool Relation<type>::irreflexive() {
+// }
+// template <typename type>
+// bool Relation<type>::reflexive() {
 
-}
+// }
+// template <typename type>
+// bool Relation<type>::irreflexive() {
+
+// }
 
 
-template <typename type>
-bool Relation<type>::transitive() {
+// template <typename type>
+// bool Relation<type>::transitive() {
 
-}
-template <typename type>
-bool Relation<type>::is_function() {
+// }
+// template <typename type>
+// bool Relation<type>::is_function() {
 
-}
+// }
 
 
-template <typename type>
-Relation<type>Relation<type>::inverse() {
+// template <typename type>
+// Relation<type>Relation<type>::inverse() {
 
-}
-template <typename type>
-Relation<type>Relation<type>::combination() {
+// }
+// template <typename type>
+// Relation<type>Relation<type>::combination() {
 
-}
+// }
 
 int main() {
     Relation <int> R;
     pair <int, int> p;
     R.add_to_set(15);
     R.add_to_set(7);
-    R.add_to_set(8);
-    R.add_to_set(9);
-    R.add_to_set(6);
-
-    pair <int, int> p1(15,7);
-    pair <int, int> p2(6,9);
-    R.add_element(p1);
-    R.add_element(p2);
-
+    R.add_to_set(21);
+    
+    R.add_element(15, 7);
+    R.add_element(21, 21);
 
     cout << R << endl;
 
