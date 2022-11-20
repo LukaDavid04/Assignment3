bbd2b921d3e0b7fb8ca3ec3fd2215083758140e3
MichaelBosciglio <55165965+MichaelBosciglio@users.noreply.github.com>
2022-11-19 19:02:19 -0500

    MappingRelation

--- MappingRelation.cpp
+++ MappingRelation.cpp
@@ -3,12 +3,14 @@
 #include <set>
 #include <utility>
 #include "MappingRelation.h"
+#include <list>
+#include <vector>
 
 using namespace std;
 
-template <typename typex, typename typey, typename typez>
-ostream& operator << (ostream& out, const MappingRelation<typex, typey, typez>& s) {
-    typename set<pair<typex, typex>>::iterator it;
+template <typename typex, typename typey>
+ostream& operator << (ostream& out, const MappingRelation<typex, typey>& s) {
+    typename set<pair<typex, typey>>::iterator it;
 
     out << "{ ";
     for (it = s.relations.begin(); it != s.relations.end(); it++) {
@@ -24,141 +26,144 @@ ostream& operator << (ostream& out, const MappingRelation<typex, typey, typez>& 
     return out;
 }
 
-template <typename type, typename type1, typename type2>
-MappingRelation<type, type1, type2>::MappingRelation() {
+template <typename type1, typename type2>
+MappingRelation<type1, type2>::MappingRelation() {
     int size = 0;
-    set <pair<type, type>> relations;
-    set <type> root;
+    set <pair<type1, type2>> relations;
+    set <type1> root1;
+    set <type2> root2;
 }
 
-template <typename type, typename type1, typename type2>
-MappingRelation<type, type1, type2>::MappingRelation(const MappingRelation& r) {
+template <typename type1, typename type2>
+MappingRelation<type1, type2>::MappingRelation(const MappingRelation& r) {
     relations = r.relations;
-    root = r.root;
+    root1 = r.root1;
+    root2 = r.root2;
     size = r.size;
 }
 
-template <typename type, typename type1, typename type2>
-MappingRelation<type, type1, type2>::~MappingRelation() {
+template <typename type1, typename type2>
+MappingRelation<type1, type2>::~MappingRelation() {
     relations.clear();
-    root.clear();
+    root1.clear();
+    root2.clear();
 }
 
-template <typename type, typename type1, typename type2>
-int MappingRelation<type, type1, type2>::cardinality() {
-    return size;
+template <typename type1, typename type2>
+int MappingRelation<type1, type2>::cardinality() {
+    return relations.size();
 }
 
-template <typename type, typename type1, typename type2>
-bool MappingRelation<type, type1, type2>::add_element(pair<type1, type2> p) {
-    int i;
-
-    if (root.count(p.first) < 1 || root.count(p.second) < 1)
-        return false;
-
-    relations.insert(p);
+template <typename type1, typename type2>
+bool MappingRelation<type1, type2>::add_element(type1 x, type2 y) {
+    pair<type1, type2> temp;
+    temp.first = x;
+    temp.second = y;
+    relations.insert(temp);
     size += 1;
     return true;
 }
 
-template <typename type, typename type1, typename type2>
-bool MappingRelation<type, type1, type2>::add_to_set(type i) {
-    if (root.count(i) > 0) return false;
-    else root.insert(i);
-    return true;
+template <typename type1, typename type2>
+void MappingRelation<type1, type2>::add_to_set(type1 x, type2 y) {
+    root1.insert(x);
+    root2.insert(y);
 }
 
-template <typename type, typename type1, typename type2>
-void MappingRelation<type, type1, type2>::remove_element(pair<type1, type2> p) {
-    /*relations.erase(p);
-    typename set <pair<type, type>>::iterator pi;
-    for (pi = relations.begin(); pi != relations.end(); ++pi)
-    {
-        if (pi.count(pi.first) < 2 ) root.erase(pi.first);
-        if (pi.count(pi.second) < 2) root.erase(pi.second);
-
-    }
-    root.erase(p);
-    --size;*/
+template <typename type1, typename type2>
+void MappingRelation<type1, type2>::remove_element(type1 x, type2 y) {
+    pair <type1, type2> p(x, y);
+    relations.erase(p);
 }
 
-template <typename type, typename type1, typename type2>
-bool MappingRelation<type, type1, type2>::is_member(pair <type1, type2> p) {
-    if (relations.count(p) > 0) return true;
-    return false;
+template <typename type1, typename type2>
+bool MappingRelation<type1, type2>::is_member(type1 x, type2 y) {
+    typename set<pair<type1, type2>>::iterator it;
+    for (it = relations.begin(); it != relations.end(); it++) if (it->first == x && it->second == y)return true; return false;
 }
 
-template <typename type, typename type1, typename type2>
-bool MappingRelation<type, type1, type2>::equal(MappingRelation r) {
-    typename set <pair<type, type>>::iterator pi;
-    for (pi = relations.begin(); pi != relations.end(); ++pi)
-    {
-        cout << "Cool";
+template <typename type1, typename type2>
+bool MappingRelation<type1,type2>::operator ==(MappingRelation<type1, type2> x){
+    typename set<pair<type1, type2>>::iterator it;
+    typename set<pair<type1, type2>>::iterator it2;
 
+    for (it = x.relations.begin(); it != x.relations.end(); it++){
+        for (it2 = relations.begin(); it2 != relations.end(); it++){
+            if (it != relations.end()) return false;
+        }
     }
     return true;
 }
 
-// template <typename type, typename type1, typename type2>
-// bool MappingRelation<type, type1, type2>::symmetric() {
+template <typename type1, typename type2>
+MappingRelation<type1, type2>& MappingRelation<type1,type2>::operator +(MappingRelation<type1, type2> x){
+
+    typename set<pair<type1, type2>>::iterator it;
+    pair <type1, type2> p;
+
+    for (it = x.relations.begin(); it != x.relations.end(); it++){
+        
+        p.first = it->first;
+        p.second = it->second;
+
+        relations.insert(p);
+    } 
+
+    return (*this);
+}
+
+template <typename type1, typename type2>
+MappingRelation<type1, type2>& MappingRelation<type1,type2>::operator -(MappingRelation<type1, type2>){
+
+}
+
+template <typename type1, typename type2>
+vector<type1> MappingRelation<type1, type2>::operator [](type1 x){
+    vector<type1> temp;
+    typename set<pair<type1, type2>>::iterator it;
+    for (it = relations.begin(); it != relations.end(); it++){
+        
+        if (it->first == x){
+            temp.push_back(it->second);
+        } 
+    } 
+    return temp;
+}
+
+// template <typename type1, typename type2>
+// bool MappingRelation<type1, type2>::intersection() {
 
 // }
 
-// template <typename type, typename type1, typename type2>
-// bool MappingRelation<type, type1, type2>::asymmetric() {
+// template <typename type1, typename type2>
+// bool MappingRelation<type1, type2>::is_function() {
 
 // }
 
-// template <typename type, typename type1, typename type2>
-// bool MappingRelation<type, type1, type2>::reflexive() {
+// template <typename type1, typename type2>
+// MappingRelation<type1, type2>MappingRelation<type1, type2>::inverse() {
 
 // }
 
-// template <typename type, typename type1, typename type2>
-// bool MappingRelation<type, type1, type2>::irreflexive() {
-
-// }
-
-// template <typename type, typename type1, typename type2>
-// bool MappingRelation<type, type1, type2>::transitive() {
-
-// }
-
-// template <typename type, typename type1, typename type2>
-// bool MappingRelation<type, type1, type2>::is_function() {
-
-// }
-
-// template <typename type, typename type1, typename type2>
-// MappingRelation<type, type1, type2>MappingRelation<type, type1, type2>::inverse() {
-
-// }
-
-// template <typename type, typename type1, typename type2>
-// MappingRelation<type, type1, type2>MappingRelation<type, type1, type2>::combination() {
+// template <typename type1, typename type2>
+// MappingRelation<type1, type2>MappingRelation<type1, type2>::combination() {
     
 // }
 
-int main() {
-    MappingRelation <int, int, int> R;
+// int main() {
+//     MappingRelation <int, int> R;
 
-    pair <int, int> p;
-    R.add_to_set(15);
-    R.add_to_set(7);
-    R.add_to_set(8);
-    R.add_to_set(9);
-    R.add_to_set(6);
+//     pair <int, int> p;
+//     pair <int, int> p1(15,7);
+//     pair <int, int> p2(6,9);
+//     R.add_element(p1.first, p1.second);
+//     R.add_element(p2.first, p2.second);
 
-    pair <int, int> p1(15,7);
-    pair <int, int> p2(6,9);
-    R.add_element(p1);
-    R.add_element(p2);
+//     MappingRelation <int, int> P(R);
 
-    MappingRelation <int, int, int> P(R);
-
-    cout << R << endl;
-    cout << P << endl;
+//     cout << R << endl;
+//     cout << P << endl;
 
 
-    return 0;
-}
+//     return 0;
+// }
--- MappingRelation.h
+++ MappingRelation.h
@@ -2,33 +2,35 @@
 #include <iostream>
 #include <utility>
 #include <set>
+#include <list>
+#include <vector>
 
 using namespace std;
 
-template <typename type, typename type1, typename type2>
+template <typename type1, typename type2>
 class MappingRelation {
 private:
     set <pair<type1, type2>> relations;
-    set <type> root;
+    set <type1> root1;
+    set <type2> root2;
     int size;
 public:
     MappingRelation();
     MappingRelation(const MappingRelation&);
     ~MappingRelation();
     int cardinality();
-    bool add_element(pair <type1, type2>);
-    bool add_to_set(type);
-    void remove_element(pair <type1, type2>);
-    bool is_member(pair <type1, type2>);
-    bool equal(MappingRelation);
-    bool reflexive();
-    bool irreflexive();
-    bool symmetric();
-    bool asymmetric();
-    bool transitive();
+    bool add_element(type1, type2);
+    void add_to_set(type1, type2);
+    void remove_element(type1, type2);
+    bool is_member(type1, type2);
+    bool operator ==(MappingRelation);
+    MappingRelation& operator +(MappingRelation);
+    MappingRelation& operator -(MappingRelation);
+    vector<type1> operator [](type1);
     bool is_function();
+    bool intersection();
     MappingRelation inverse();
     MappingRelation combination();
-    template <typename typex, typename typey, typename typez>
-    friend ostream& operator << (ostream&, const MappingRelation<typex, typey, typez>&);
+    template <typename typex, typename typey>
+    friend ostream& operator << (ostream&, const MappingRelation<typex, typey>&);
 };
